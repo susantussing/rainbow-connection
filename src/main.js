07b95e25057cb88d68f616daeab1ba60dc7ef636
Susan Tussing <susan@fionavar.net>
2017-08-10 10:55:11 -0500

    Install Vuex

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App.vue'
 import tinycolor from 'tinycolor2'
 import axios from 'axios'
+
+Vue.use(Vuex)
 
 new Vue({
   el: '#app',
